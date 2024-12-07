487cdab7b4d42bfe358874bcf4b4180370be1c62
Agena <103839428+Agena7400@users.noreply.github.com>
2024-12-07 23:14:30 +0900

    file create

---
+++ Kudryavka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Device.Gpio;
+
+namespace Kudryavka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
