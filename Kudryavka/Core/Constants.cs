91903bd14f14297168163ec0d7d201a475e9d0c8
Agena <103839428+Agena7400@users.noreply.github.com>
2024-12-08 11:34:20 +0900

    added System.Device.Gpio library

---
+++ Kudryavka/Core/Constants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Device.Gpio;
+using System.Reflection;
+
+namespace Kudryavka.Core
+{
+    internal static class Constants
+    {
+        //アプリケーション名
+        public static string ApplicationName = "Kudryavka";
+        //アプリケーションバージョン
+        public static string ApplicationVersion = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown Version";
+        //開発者情報
+        public static string Developer = "SARD";
+        public static string Coding = "Shintaro Niwa";
+
+        //GPIOの設定
+        //ピンアサイン
+        public const int PinNumber = 17;
+    }
+}
